92a133850e4db5da67ef9265cb28b1b01b9cdaf4
Albert-Jan <albertjan@curit.com>
2011-08-22 22:23:24 +0200

    Start of a simple configuration interface

---
+++ NP2COMV/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace NP2COMV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox ();
+            this.dataBitsComboBox = new System.Windows.Forms.ComboBox ();
+            this.stopBitsComboBox = new System.Windows.Forms.ComboBox ();
+            this.parityComboBox = new System.Windows.Forms.ComboBox ();
+            this.label6 = new System.Windows.Forms.Label ();
+            this.label5 = new System.Windows.Forms.Label ();
+            this.label4 = new System.Windows.Forms.Label ();
+            this.label3 = new System.Windows.Forms.Label ();
+            this.baudRateComboBox = new System.Windows.Forms.ComboBox ();
+            this.label2 = new System.Windows.Forms.Label ();
+            this.serialPortComboBox = new System.Windows.Forms.ComboBox ();
+            this.groupBox2 = new System.Windows.Forms.GroupBox ();
+            this.label1 = new System.Windows.Forms.Label ();
+            this.namedPipeComboBox = new System.Windows.Forms.ComboBox ();
+            this.button1 = new System.Windows.Forms.Button ();
+            this.button2 = new System.Windows.Forms.Button ();
+            this.label7 = new System.Windows.Forms.Label ();
+            this.label8 = new System.Windows.Forms.Label ();
+            this.groupBox1.SuspendLayout ();
+            this.groupBox2.SuspendLayout ();
+            this.SuspendLayout ();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add (this.dataBitsComboBox);
+            this.groupBox1.Controls.Add (this.stopBitsComboBox);
+            this.groupBox1.Controls.Add (this.parityComboBox);
+            this.groupBox1.Controls.Add (this.label6);
+            this.groupBox1.Controls.Add (this.label5);
+            this.groupBox1.Controls.Add (this.label4);
+            this.groupBox1.Controls.Add (this.label3);
+            this.groupBox1.Controls.Add (this.baudRateComboBox);
+            this.groupBox1.Controls.Add (this.label2);
+            this.groupBox1.Controls.Add (this.serialPortComboBox);
+            this.groupBox1.Location = new System.Drawing.Point (298, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size (280, 249);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SerialPort:";
+            // 
+            // dataBitsComboBox
+            // 
+            this.dataBitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dataBitsComboBox.FormattingEnabled = true;
+            this.dataBitsComboBox.Items.AddRange (new object[] {
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.dataBitsComboBox.Location = new System.Drawing.Point (64, 100);
+            this.dataBitsComboBox.Name = "dataBitsComboBox";
+            this.dataBitsComboBox.Size = new System.Drawing.Size (210, 21);
+            this.dataBitsComboBox.TabIndex = 11;
+            // 
+            // stopBitsComboBox
+            // 
+            this.stopBitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stopBitsComboBox.FormattingEnabled = true;
+            this.stopBitsComboBox.Location = new System.Drawing.Point (64, 127);
+            this.stopBitsComboBox.Name = "stopBitsComboBox";
+            this.stopBitsComboBox.Size = new System.Drawing.Size (210, 21);
+            this.stopBitsComboBox.TabIndex = 10;
+            // 
+            // parityComboBox
+            // 
+            this.parityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.parityComboBox.FormattingEnabled = true;
+            this.parityComboBox.Location = new System.Drawing.Point (64, 73);
+            this.parityComboBox.Name = "parityComboBox";
+            this.parityComboBox.Size = new System.Drawing.Size (210, 21);
+            this.parityComboBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point (6, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size (49, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "StopBits:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point (6, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size (50, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "DataBits:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point (6, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size (36, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Parity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point (6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size (53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Baudrate:";
+            // 
+            // baudRateComboBox
+            // 
+            this.baudRateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudRateComboBox.FormattingEnabled = true;
+            this.baudRateComboBox.Items.AddRange (new object[] {
+            "110 ",
+            "300 ",
+            "600 ",
+            "1200 ",
+            "2400 ",
+            "4800 ",
+            "9600 ",
+            "14400 ",
+            "19200 ",
+            "28800 ",
+            "38400 ",
+            "56000 ",
+            "57600 ",
+            "115200 ",
+            "128000 ",
+            "153600 ",
+            "230400 ",
+            "256000 ",
+            "460800",
+            "921600"});
+            this.baudRateComboBox.Location = new System.Drawing.Point (64, 46);
+            this.baudRateComboBox.Name = "baudRateComboBox";
+            this.baudRateComboBox.Size = new System.Drawing.Size (210, 21);
+            this.baudRateComboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point (6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size (55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SerialPort:";
+            // 
+            // serialPortComboBox
+            // 
+            this.serialPortComboBox.FormattingEnabled = true;
+            this.serialPortComboBox.Location = new System.Drawing.Point (64, 19);
+            this.serialPortComboBox.Name = "serialPortComboBox";
+            this.serialPortComboBox.Size = new System.Drawing.Size (210, 21);
+            this.serialPortComboBox.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add (this.label8);
+            this.groupBox2.Controls.Add (this.label7);
+            this.groupBox2.Controls.Add (this.label1);
+            this.groupBox2.Controls.Add (this.namedPipeComboBox);
+            this.groupBox2.Location = new System.Drawing.Point (12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size (280, 249);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "NamedPipe";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point (6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size (65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NamedPipe:";
+            // 
+            // namedPipeComboBox
+            // 
+            this.namedPipeComboBox.FormattingEnabled = true;
+            this.namedPipeComboBox.Location = new System.Drawing.Point (77, 19);
+            this.namedPipeComboBox.Name = "namedPipeComboBox";
+            this.namedPipeComboBox.Size = new System.Drawing.Size (197, 21);
+            this.namedPipeComboBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point (584, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size (126, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler (this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point (584, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size (126, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Write config file";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point (6, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size (257, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Named pipe clients are configured to be BiDirectional";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point (6, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size (95, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "and Asynchronous";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size (722, 273);
+            this.Controls.Add (this.button2);
+            this.Controls.Add (this.button1);
+            this.Controls.Add (this.groupBox2);
+            this.Controls.Add (this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler (this.Form1_Load);
+            this.groupBox1.ResumeLayout (false);
+            this.groupBox1.PerformLayout ();
+            this.groupBox2.ResumeLayout (false);
+            this.groupBox2.PerformLayout ();
+            this.ResumeLayout (false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox serialPortComboBox;
+        private System.Windows.Forms.ComboBox namedPipeComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox baudRateComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox parityComboBox;
+        private System.Windows.Forms.ComboBox stopBitsComboBox;
+        private System.Windows.Forms.ComboBox dataBitsComboBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ NP2COMV/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NP2COM;
+
+
+namespace NP2COMV
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent ();
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            namedPipeComboBox.Items.AddRange(Directory.GetFiles(@"\\.\pipe\"));
+            serialPortComboBox.Items.AddRange(SerialPort.GetPortNames());
+            parityComboBox.Items.AddRange(Enum.GetNames(typeof(Parity)));
+            stopBitsComboBox.Items.AddRange(Enum.GetNames(typeof(StopBits)));
+            if (namedPipeComboBox.Items.Count > 0) namedPipeComboBox.SelectedIndex = 0;
+            if (serialPortComboBox.Items.Count > 0) serialPortComboBox.SelectedIndex = 0;
+            baudRateComboBox.SelectedIndex = 10;
+            parityComboBox.SelectedIndex = 0;
+            dataBitsComboBox.SelectedIndex = 3;
+            stopBitsComboBox.SelectedIndex = 1;
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            new Connection(new Settings
+                               {
+                                   BaudRate = (int)baudRateComboBox.SelectedItem,
+                                   ComPort = (string)serialPortComboBox.SelectedItem,
+                               });
+        }
+
+    }
+}
